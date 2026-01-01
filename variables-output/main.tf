85ffb72126f81d4336fc705d16fb1a212ebfba2d
Suleyman Akman <suleymanakman@gmail.com>
2026-01-01 19:07:21 +0300

    import ec2

--- variables-output/main.tf
+++ variables-output/main.tf
@@ -17,3 +17,23 @@ resource "aws_instance" "import" {
   
   }
 }
+
+resource "aws_instance" "import2" {
+  ami           = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name        = "deneme"
+  
+  }
+}
+
+resource "aws_instance" "import3" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name        = "my-EC2-instance-terraform.auto.tfvars"
+  
+  }
+}
