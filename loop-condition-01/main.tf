59bd020fb6e2a29d67f87ba45628b05cc8ff21f7
Suleyman Akman <suleymanakman@gmail.com>
2026-01-02 18:56:17 +0300

    loop condition devam

--- loop-condition-01/main.tf
+++ loop-condition-01/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "main" {
+  count         = var.is_production ? 3 : 1
   ami           = "ami-043a5a82b6cf98947"
   instance_type = var.is_production ? "t2.large" : "t2.micro"
 
   tags = {
-    Name        = var.ec2_name
+    Name        = "${var.ec2_name}-${count.index + 1}"
     Environment = var.is_production ? "Production" : "Development"
   }
 }
--- loop-condition-01/output.tf
+++ loop-condition-01/output.tf
@@ -1,15 +1,15 @@
-output "ami" {
-  value = aws_instance.main.ami
+output "instance_ami" {
+  value = aws_instance.main.*.ami
 }
 
-output "instance_id" {
-  value = aws_instance.main.id
+output "instance_ids" {
+  value = aws_instance.main.*.id
 }
 
-output "instance_ip" {
-  value = aws_instance.main.private_ip
+output "instance_ips" {
+  value = aws_instance.main.*.private_ip
 }
 
 output "instance_tags" {
-  value = aws_instance.main.tags_all
+  value = aws_instance.main.*.tags_all
 }
